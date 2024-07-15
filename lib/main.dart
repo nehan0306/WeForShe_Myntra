486d0edcf374f4d7974cb4eef762ff326f14f414
bhargavi0401 <99488704+bhargavi0401@users.noreply.github.com>
2024-07-15 23:06:37 +0530

    bottom navigation bar added

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SupabaseClient supabaseClient = SupabaseClient(
     'https://mlqtdjmsspmsinudegsl.supabase.co', // Your Supabase URL
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1scXRkam1zc3Btc2ludWRlZ3NsIiwicm9sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA4MDk2NjksImV4cCI6MjAzNjM4NTY2OX0.CP6Cvi8bCdaGduViiX6mMEhY_GbOSWzcxYAZL6n5rQU', // Your Supabase anonymous key
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1scXRkam1zc3Btc2ludWRlZ3NsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA4MDk2NjksImV4cCI6MjAzNjM4NTY2OX0.CP6Cvi8bCdaGduViiX6mMEhY_GbOSWzcxYAZL6n5rQU', // Your Supabase anonymous key
   );
   runApp(MyApp(supabaseClient: supabaseClient));
 }
